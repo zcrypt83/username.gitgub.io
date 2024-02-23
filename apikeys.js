5eaf526105a220a74ae3d52fe9946eb1973d85cd
ivanzarate12 <130786431+ivanzarate12@users.noreply.github.com>
2024-02-22 23:47:33 -0500

    Update apikeys.js

--- apikeys.js
+++ apikeys.js
@@ -1,4 +1,4 @@
 // Rename this script to "apikeys.js" and add your OpenAI API key below
 // IMPORTANT: DO NOT upload the "apikeys.js" file to any server. (see readme.md for more info)
-const openai_apikey = "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";
+const openai_apikey = "sk-";
 
